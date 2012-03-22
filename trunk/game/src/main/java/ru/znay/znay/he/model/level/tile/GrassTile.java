d3d3b60c9ce5907c28ddc995825009a5f8fa87df
mr.gmaker@yandex.ru <mr.gmaker@yandex.ru@5dbda700-67c0-00fe-5b07-dab74de87440>
2012-03-22 12:24:02 +0000

    Fixed water

--- trunk/game/src/main/java/ru/znay/znay/he/model/level/tile/GrassTile.java
+++ trunk/game/src/main/java/ru/znay/znay/he/model/level/tile/GrassTile.java
@@ -1,5 +1,6 @@
 package ru.znay.znay.he.model.level.tile;
 
+import ru.znay.znay.he.gfx.helper.PaletteHelper;
 import ru.znay.znay.he.gfx.model.Screen;
 import ru.znay.znay.he.model.level.Level;
 
@@ -23,10 +24,32 @@ public class GrassTile extends Tile {
 
     @Override
     public void render(Screen screen, Level level, int x, int y) {
-        screen.render(x * SIZE, y * SIZE, 0 * HALF_SIZE, 0, HALF_SIZE, HALF_SIZE, grassColor, 0);
-        screen.render(x * SIZE + HALF_SIZE, y * SIZE, 1 * HALF_SIZE, 0, HALF_SIZE, HALF_SIZE, grassColor, 1);
-        screen.render(x * SIZE, y * SIZE + HALF_SIZE, 2 * HALF_SIZE, 0, HALF_SIZE, HALF_SIZE, grassColor, 2);
-        screen.render(x * SIZE + HALF_SIZE, y * SIZE + HALF_SIZE, 3 * HALF_SIZE, 0, HALF_SIZE, HALF_SIZE, grassColor, 3);
+
+        int transitionColor = PaletteHelper.getColor(grassMainColor - 111, grassMainColor, grassMainColor + 111, dirtMainColor);
+
+        boolean u = !level.getTile(x, y - 1).connectsToGrass;
+        boolean d = !level.getTile(x, y + 1).connectsToGrass;
+        boolean l = !level.getTile(x - 1, y).connectsToGrass;
+        boolean r = !level.getTile(x + 1, y).connectsToGrass;
+
+        if (!u && !l) {
+            screen.render(x * Tile.SIZE + 0, y * Tile.SIZE + 0, 0, 0, Tile.HALF_SIZE, Tile.HALF_SIZE, grassColor, 0);
+        } else
+            screen.render(x * Tile.SIZE + 0, y * Tile.SIZE + 0, (l ? 11 : 12) * Tile.HALF_SIZE, (u ? 0 : 1) * Tile.HALF_SIZE, Tile.HALF_SIZE, Tile.HALF_SIZE, transitionColor, 0);
+
+        if (!u && !r) {
+            screen.render(x * Tile.SIZE + Tile.HALF_SIZE, y * Tile.SIZE + 0, 1 * Tile.HALF_SIZE, 0, Tile.HALF_SIZE, Tile.HALF_SIZE, grassColor, 0);
+        } else
+            screen.render(x * Tile.SIZE + Tile.HALF_SIZE, y * Tile.SIZE + 0, (r ? 13 : 12) * Tile.HALF_SIZE, (u ? 0 : 1) * Tile.HALF_SIZE, Tile.HALF_SIZE, Tile.HALF_SIZE, transitionColor, 0);
+
+        if (!d && !l) {
+            screen.render(x * Tile.SIZE + 0, y * Tile.SIZE + Tile.HALF_SIZE, 2 * Tile.HALF_SIZE, 0, Tile.HALF_SIZE, Tile.HALF_SIZE, grassColor, 0);
+        } else
+            screen.render(x * Tile.SIZE + 0, y * Tile.SIZE + Tile.HALF_SIZE, (l ? 11 : 12) * Tile.HALF_SIZE, (d ? 2 : 1) * Tile.HALF_SIZE, Tile.HALF_SIZE, Tile.HALF_SIZE, transitionColor, 0);
+        if (!d && !r) {
+            screen.render(x * Tile.SIZE + Tile.HALF_SIZE, y * Tile.SIZE + Tile.HALF_SIZE, 3 * Tile.HALF_SIZE, 0, Tile.HALF_SIZE, Tile.HALF_SIZE, grassColor, 0);
+        } else
+            screen.render(x * Tile.SIZE + Tile.HALF_SIZE, y * Tile.SIZE + Tile.HALF_SIZE, (r ? 13 : 12) * Tile.HALF_SIZE, (d ? 2 : 1) * Tile.HALF_SIZE, Tile.HALF_SIZE, Tile.HALF_SIZE, transitionColor, 0);
     }
 
     @Override
