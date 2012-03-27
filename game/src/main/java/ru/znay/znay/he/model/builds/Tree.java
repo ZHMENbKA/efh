e149b8e62a07894735266d099bc90b125852f884
denuss@gmail.com <denuss@gmail.com@5dbda700-67c0-00fe-5b07-dab74de87440>
2012-03-27 18:54:30 +0000

    изменение иерархии для деревьев, добавил абстрактный класс Tree от которого нужно наследовать деревья\пеньки\кусты

--- game/src/main/java/ru/znay/znay/he/model/builds/Tree.java
+++ game/src/main/java/ru/znay/znay/he/model/builds/Tree.java
@@ -1,30 +1,26 @@
 package ru.znay.znay.he.model.builds;
 
-import ru.znay.znay.he.Game;
 import ru.znay.znay.he.gfx.helper.BitmapHelper;
-import ru.znay.znay.he.gfx.helper.PaletteHelper;
 import ru.znay.znay.he.gfx.helper.SpriteManager;
 import ru.znay.znay.he.gfx.helper.StaticModel;
-import ru.znay.znay.he.gfx.model.Bitmap;
 import ru.znay.znay.he.gfx.model.Screen;
 import ru.znay.znay.he.model.Entity;
 import ru.znay.znay.he.model.Mob;
-import ru.znay.znay.he.model.level.tile.Tile;
 
 /**
  * Created by IntelliJ IDEA.
  * User: denus
- * Date: 22.03.12
- * Time: 17:47
+ * Date: 27.03.12
+ * Time: 22:41
  * To change this template use File | Settings | File Templates.
  */
-public class AppleTree extends Tree {
+public abstract class Tree extends Mob {
+    public Tree(int x, int y) {
+        this.x = x;
 
-    private int apple = random.nextInt(2);
-
-    public AppleTree(int x, int y, SpriteManager spr) {
-        super(x,y);
-        sprite = spr.getSprite((apple == 0) ? StaticModel.Apple : StaticModel.Apple2);
+        this.y = y;
+        this.xr = 16;
+        this.yr = 12;
     }
 
 
@@ -38,4 +34,12 @@ public class AppleTree extends Tree {
         return true;
     }
 
+    public void render(Screen screen) {
+
+        int xt = (x - xr * 2) - screen.getXOffset();
+        int yt = (y - yr * 2 - 24) - screen.getYOffset();
+
+        BitmapHelper.drawNormal(sprite, xt, yt, screen.getViewPort(), 0xFF00FF);
+
+    }
 }
