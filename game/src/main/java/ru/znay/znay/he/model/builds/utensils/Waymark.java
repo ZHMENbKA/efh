b19b519f4abc013e9f4559472003b4bd0b77ce68
denuss@gmail.com <denuss@gmail.com@5dbda700-67c0-00fe-5b07-dab74de87440>
2012-04-15 13:07:59 +0000

    загрузка сообщений из файла

---
+++ game/src/main/java/ru/znay/znay/he/model/builds/utensils/Waymark.java
@@ -0,0 +1,39 @@
+package ru.znay.znay.he.model.builds.utensils;
+
+import ru.znay.znay.he.gfx.gui.GuiManager;
+import ru.znay.znay.he.gfx.gui.Menu;
+import ru.znay.znay.he.gfx.gui.TypedTextPanel;
+import ru.znay.znay.he.gfx.helper.PaletteHelper;
+import ru.znay.znay.he.gfx.sprite.SpriteCollector;
+import ru.znay.znay.he.gfx.sprite.SpriteWrapper;
+import ru.znay.znay.he.model.Entity;
+import ru.znay.znay.he.model.level.tile.Tile;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Денис Сергеевич
+ * Date: 15.04.12
+ * Time: 12:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class Waymark extends Utensils {
+    private long tick;
+    private String message;
+    public Waymark(int x, int y, SpriteCollector spriteCollector,String message) {
+        super(x, y, 4, 1);
+         this.message = message;
+        spriteCollector.resetWrappers();
+        spriteCollector.addWrapper(new SpriteWrapper(10 * Tile.HALF_SIZE, 5 * Tile.HALF_SIZE, Tile.HALF_SIZE << 1, Tile.HALF_SIZE << 1, PaletteHelper.getColor(320, 421, 430, -1)));
+        this.sprite = spriteCollector.mergedWrappers("waymark", 1, 0, true);
+    }
+
+    @Override
+    public void touchedBy(Entity entity) {
+        super.touchedBy(entity);
+
+        if (tick > System.currentTimeMillis()) return;
+
+        tick = System.currentTimeMillis() +1000;
+        GuiManager.getInstance().add(new TypedTextPanel(message,4,4,100),"waymark");
+    }
+}
