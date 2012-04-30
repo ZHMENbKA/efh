c53f60acb0110cb816bd24422ea78c0ddd44211a
denuss@gmail.com <denuss@gmail.com@5dbda700-67c0-00fe-5b07-dab74de87440>
2012-04-30 12:45:46 +0000

    правка колодца

--- trunk/game/src/main/java/ru/znay/znay/he/model/builds/utensils/Well.java
+++ trunk/game/src/main/java/ru/znay/znay/he/model/builds/utensils/Well.java
@@ -1,6 +1,7 @@
 package ru.znay.znay.he.model.builds.utensils;
 
 import ru.znay.znay.he.gfx.helper.PaletteHelper;
+import ru.znay.znay.he.gfx.sprite.SpriteCollector;
 import ru.znay.znay.he.gfx.sprite.SpriteWrapper;
 import ru.znay.znay.he.model.level.tile.Tile;
 
@@ -12,13 +13,13 @@ import ru.znay.znay.he.model.level.tile.Tile;
  * To change this template use File | Settings | File Templates.
  */
 public class Well extends Utensils {
-    public Well(int x, int y) {
+    public Well(int x, int y, SpriteCollector spriteCollector) {
         super(x, y, 4, 1);
 
-        level.getSpriteCollector().resetWrappers();
-        level.getSpriteCollector().addWrapper(new SpriteWrapper(0, Tile.HALF_SIZE, Tile.HALF_SIZE << 1, Tile.HALF_SIZE << 1, PaletteHelper.getColor(111, 222, 333, -1)));
-        level.getSpriteCollector().addWrapper(new SpriteWrapper(2 * Tile.HALF_SIZE, Tile.HALF_SIZE, Tile.HALF_SIZE << 1, Tile.HALF_SIZE << 1, PaletteHelper.getColor(2, 3, 4, -1)));
+        spriteCollector.resetWrappers();
+        spriteCollector.addWrapper(new SpriteWrapper(0, Tile.HALF_SIZE, Tile.HALF_SIZE << 1, Tile.HALF_SIZE << 1, PaletteHelper.getColor(111, 222, 333, -1)));
+        spriteCollector.addWrapper(new SpriteWrapper(2 * Tile.HALF_SIZE, Tile.HALF_SIZE, Tile.HALF_SIZE << 1, Tile.HALF_SIZE << 1, PaletteHelper.getColor(2, 3, 4, -1)));
 
-        this.sprite = level.getSpriteCollector().mergedWrappers("well", 1, 0, 0);
+        this.sprite = spriteCollector.mergedWrappers("well", 1, 0, 0);
     }
 }
